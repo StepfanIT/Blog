22258bca2e209b1a4d3d6071c2a0b7af95a6fd8c
Stepan <svatoslavsemcuk751@gmail.com>
2025-03-16 20:36:22 +0200

    add frontend(first)

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import post_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', post_list, name='post_list'),
 ]
