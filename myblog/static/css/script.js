d67afe2a2c2c2ec59acddd796bf09f9e4d642eb6
Stepan <svatoslavsemcuk751@gmail.com>
2025-03-18 23:57:03 +0200

    Add adaptation frontend

---
+++ myblog/static/css/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.getElementById('burger');
+    const navLinks = document.getElementById('nav-links');
+
+    burger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
